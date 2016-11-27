fb02acf8f7729e92fd17a7770d43ee546cf94c77
Adam DuQuette <duquetteadam@gmail.com>
2016-11-27 09:58:43 -0800

    Fixed pythong formatting error, setup test code for connecting to server socket from client

--- ftclient.cpp
+++ ftclient.cpp
@@ -1,8 +1,53 @@
 #include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netdb.h>
 
 int main(int argc, char **argv)
 {
-	std::cout << "Hello world\n";
+  struct sockaddr_in servAddr;
+  struct hostent *server;
+  
+  //Create the client socket
+  int client = socket(AF_INET, SOCK_STREAM, 0);
+
+  //Verify socket was created, print message to std with result
+  if(client < 0)
+  {
+      std::cout << "ERROR: Unable to create client socket.\n";
+      return 1;
+  }else
+  {
+      std::cout << "Client socket created successfully.\n";
+  }
+
+  //Define server info that we are connecting to
+  servAddr.sin_family = AF_INET;
+  servAddr.sin_port = htons(atoi(argv[2]));
+
+  server = gethostbyname(argv[1]);
+  bcopy((char *)server->h_addr, (char *)&servAddr.sin_addr.s_addr, server->h_length);
+
+
+  //Attempt to make a connection
+  int res = connect(client, (struct sockaddr *)&servAddr, sizeof(servAddr));
+
+  //Make sure it connected, otherwise exit
+  if(res == 0)
+      std::cout << "Connection established on port number: " << argv[2] << std::endl;
+  else{
+      std::cout << "ERROR: Connection could not be established on port number: " << argv[2] << std::endl;
+      close(client);
+      return 1;
+  }
+
+  close(client);
 
 	return 0;
 }
--- serverft.py
+++ serverft.py
@@ -5,18 +5,21 @@ import socket
 import signal
 
 #make sure usage is correct
-if(len(sys.argv) != 2):
-	print "usage: ./code.py port"
-	sys.exit(1)
+if len(sys.argv) != 2:
+    print "usage: ./code.py port"
+    sys.exit(1)
 
-#Register signal and define signal function
+#
+"""
+Register signal and define signal function
+"""
 def signal_handler(signal, frame):
 
-	#CLOSE IT
-	sock.close()
+    #CLOSE IT
+    sock.close()
 
-	print "Exiting with the highest amount of grace."
-	sys.exit(0)
+    print "Exiting with the highest amount of grace."
+    sys.exit(0)
 
 
 signal.signal(signal.SIGINT, signal_handler)
@@ -29,14 +32,43 @@ print host
 print port
 
 try:
-	#Open socket and bind to desired host and port
-	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	sock.bind( (host, int(port) )
-	print "Socket created."
-	
-	#Listen with a backlog of 1
-	sock.listen(1)
+    #Open socket and bind to desired host and port
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind((host, int(port)))
 
+    print 'Socket created.' 
+
+    #Listen with a backlog of 5
+    sock.listen(1)
+
+    while True:
+
+        print "Waiting for incoming connection..."
+
+        #Accept a connect and store sock and addr
+        (conn, addr) = sock.accept()
+        name = conn.recv(500)
+
+        print 'Connection made with {0}, waiting for message...'.format(name)
+
+        conn.close()
+    
 except Exception as err:
-	#If things go a bit wonky, go ahead and let the user know that
-	print err
+    print err
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
