9c81153646fab47e6d50fff7440f032df8c9c2f9
Adam DuQuette <duquetteadam@gmail.com>
2016-11-28 17:36:20 -0800

    switched back

--- serverft.py
+++ serverft.py
@@ -25,7 +25,7 @@ def signal_handler(signal, frame):
 signal.signal(signal.SIGINT, signal_handler)
 
 # User localhost and port# passed at cmd line
-host = 'localhost'
+host = 'flip1'
 port = sys.argv[1]
 
 print host
